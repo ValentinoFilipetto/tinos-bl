1500da4c399b244eb08718162c95eba4f338fa5b
Valentino Filipetto <valefilipetto@gmail.com>
2023-10-12 17:56:30 +0200

    first commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>About me</li>
+          <li>Music</li>
+          <li>Politics</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+// explore react router for this
+
+export default Navbar;
